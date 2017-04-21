8a68fb895df91b4be68ce5671db1ae83604414b6
AlexanderRyabov <alexander.ryabov@aurea.com>
2017-04-21 14:38:40 +0300

    test examples

---
+++ src/main/java/com/aurea/antipatterns/deadcode/u1/Class1.java
@@ -0,0 +1,18 @@
+package com.aurea.antipatterns.deadcode.u1;
+
+//CHECKSTYLE:OFF
+public class Class1 {
+
+    public void method1() {
+        System.out.println("aaa");
+    }
+
+    private void method2() {
+        System.out.println("<<<<");
+    }
+
+    private void method3() {
+        System.out.println(">>>>");
+    }
+
+}
---
+++ src/main/java/com/aurea/antipatterns/deadcode/u1/Class2.java
@@ -0,0 +1,11 @@
+package com.aurea.antipatterns.deadcode.u1;
+
+//CHECKSTYLE:OFF
+public class Class2 {
+
+    public void method2() {
+        Class1 class1 = new Class1();
+        class1.method1();
+    }
+}
+
